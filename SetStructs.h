9e1c3b3ce33587a06de0151fc93026ef7f5dbcb4
jerry <515734926@qq.com>
2022-07-05 14:32:48 +0800

    modify SetStruct.h

--- SetStructs.h
+++ SetStructs.h
@@ -1,19 +1,22 @@
 // COMP2521 22T2 Assignment 1
 
 // DO NOT CHANGE THE NAME OF THIS STRUCT
-struct node {
+typedef struct node {
 	int item;           // DO NOT CHANGE/REMOVE THIS FIELD
 	struct node *left;  // DO NOT CHANGE/REMOVE THIS FIELD
 	struct node *right; // DO NOT CHANGE/REMOVE THIS FIELD
 	
 	// You may add more fields here if needed
-};
+	int height;
+} TreeNode;
+// typedef struct node TreeNode;
 
 // DO NOT CHANGE THE NAME OF THIS STRUCT
 struct set {
 	struct node *tree;  // DO NOT CHANGE/REMOVE THIS FIELD
 
 	// You may add more fields here if needed
+	int size;
 };
 
 // You may define more structs here if needed 
